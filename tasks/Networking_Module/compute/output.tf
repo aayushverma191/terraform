6e906420dc37bba88a2dcff344d326d041794751
aayushverma191 <aayushverma4481@gmail.com>
2024-12-08 18:52:59 +0530

    Update output.tf

--- tasks/Networking_Module/compute/output.tf
+++ tasks/Networking_Module/compute/output.tf
@@ -1,10 +1,10 @@
 output pub-instance-id {
   value       = aws_instance.public_instance.id
-  description = "description"
+  description = "print the public instance id"
 }
 output pvt-instance-id {
   value       = aws_instance.private_instance.id
-  description = "description"
+  description = "print the private instance id"
 }
 output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
