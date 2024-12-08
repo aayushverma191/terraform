0d65bd90cf4467991845a16868e50a8e394b1536
aayushverma191 <aayushverma4481@gmail.com>
2024-12-08 16:39:17 +0530

    Create variable.tf

---
+++ tasks/Networking_Module/security/variable.tf
@@ -0,0 +1,21 @@
+variable vpc_ninja {
+  type        = string
+}
+variable pvt_in_sg_port {
+  type        = string
+}
+variable pvt_out_sg_port {
+  type        = string
+}
+variable pub_in_sg_port {
+  type        = string
+}
+variable pub_out_sg_port {
+  type        = string
+}
+variable all_traffic_protocol {
+  type        = string
+}
+variable tcp_protocol {
+  type        = string
+}
